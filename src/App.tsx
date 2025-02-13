0889c6673774faedbbb3a1fbfeac99de49d4cfb6
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-02-12 23:13:39 -0500

    layer fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Content from "./components/Content/Content";
 function App() {
   return (
     <section className="container">
+      <h2>This is a header</h2>
       <Content />
     </section>
   );
