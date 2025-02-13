85c0687ea0fba2b3a2e96b810b4cbb0f9522878c
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-02-13 13:02:55 -0500

    date element

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,13 +3,15 @@ import "./card.css";
 interface CardProps {
   title: string;
   content: string;
+  date: string;
 }
-const Card: React.FC<CardProps> = ({ title, content }) => {
+const Card: React.FC<CardProps> = ({ title, content, date }) => {
   return (
     <div className="card">
       <div className="row">
         <img src="" alt="image" />
       </div>
+      <p>{date}</p>
       <h2>{title}</h2>
       <p>{content}</p>
     </div>
--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -4,7 +4,7 @@ import Card from "../Card/Card";
 function Content() {
   return (
     <div className="content">
-      <Card title="Sample Title" content="Sample Content" />
+      <Card title="Sample Title" content="Sample Content" date="25 dec 2024" />
     </div>
   );
 }
