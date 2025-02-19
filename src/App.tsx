67ba44b80a15b3d75591336a081eab37ff9a0ede
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-02-19 15:18:29 -0500

    refactor router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import { RouterProvider } from "react-router-dom";
-import router from "../src/routes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import One from "./views/articles/One";
 
 function App() {
-  return <RouterProvider router={router} />;
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="one" element={<One />} />
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
