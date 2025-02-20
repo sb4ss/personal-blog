a47b4099c55a106fdb6994b9afd590ab133e9823
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-02-20 15:54:47 -0500

    background fix

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -19,6 +19,27 @@ function Content() {
         date="25 dec 2024"
         url="two"
       />
+      <Card
+        title="Sample Title"
+        content="Lorem ipsum dolorrem laboriosam magni"
+        img="test.webp"
+        date="25 dec 2024"
+        url="two"
+      />
+      <Card
+        title="Sample Title"
+        content="Lorem ipsum dolorrem laboriosam magni"
+        img="test.webp"
+        date="25 dec 2024"
+        url="two"
+      />
+      <Card
+        title="Sample Title"
+        content="Lorem ipsum dolorrem laboriosam magni"
+        img="test.webp"
+        date="25 dec 2024"
+        url="two"
+      />
     </div>
   );
 }
