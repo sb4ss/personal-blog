674edfd7acd2e31fdb2dc3d00ae57bdcb1e2299f
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-02-20 23:18:24 -0500

    delete

--- src/RouterWhitAnimation.tsx
+++ src/RouterWhitAnimation.tsx
@@ -2,7 +2,6 @@ import Home from "./views/Home.tsx";
 import One from "./views/articles/One.tsx";
 import { useLocation } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
-import Two from "./views/articles/Two.tsx";
 
 function RoutesWithAnimation() {
   const location = useLocation();
@@ -11,7 +10,6 @@ function RoutesWithAnimation() {
     <Routes location={location}>
       <Route path="/" element={<Home />} />
       <Route path="/one" element={<One />} />
-      <Route path="/two" element={<Two />} />
     </Routes>
   );
 }
