8e9b96e17bf484fa154281377d13941632209782
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-02-22 19:16:29 -0500

    discord scale

--- src/components/discord/DiscordProfile.tsx
+++ src/components/discord/DiscordProfile.tsx
@@ -1,15 +1,18 @@
 import banner from "../../../public/banner.png";
 import { DiscordPresence } from "react-discord-presence";
+import "./discord.css";
 
 function DiscordProfile() {
   return (
-    <DiscordPresence
-      args={{ developerId: "741020084113244180" }}
-      data={{
-        memberSince: new Date("8/6/2020"),
-      }}
-      formatBannerImageSrc={() => banner}
-    />
+    <div className="discord-presence">
+      <DiscordPresence
+        args={{ developerId: "741020084113244180" }}
+        data={{
+          memberSince: new Date("8/6/2020"),
+        }}
+        formatBannerImageSrc={() => banner}
+      />
+    </div>
   );
 }
 
